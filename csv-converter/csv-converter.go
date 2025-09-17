aea1b5a5732ee019ed322c89ddbfe10902fdf944
Tomasz Klosinski <tomasz.klosinski@protonmail.ch>
2025-09-17 22:28:29 +0200

    Add csv-converter

---
+++ csv-converter/csv-converter.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main() {
+	stdin := os.Stdin
+	csvReader := csv.NewReader(stdin)
+
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(records) == 0 {
+		fmt.Println("[]")
+		return
+	}
+
+	headers := records[0]
+	data := make([]map[string]string, 0, len(records)-1)
+
+	for rowIndex, row := range records[1:] {
+		if len(row) != len(headers) {
+			fmt.Fprintf(os.Stderr, "ERROR: row %d has %d columns, expected %d\n", rowIndex+1, len(row), len(headers))
+			os.Exit(1)
+		}
+
+		entry := make(map[string]string, len(headers))
+		for i, header := range headers {
+			entry[header] = row[i]
+		}
+		data = append(data, entry)
+	}
+
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(jsonData))
+}
